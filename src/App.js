d3b008bd7c930306b972ab05d90455f048e6fe60
unknown <shubhamkarb18@gmail.com>
2021-10-18 23:56:44 +0530

    Start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App=()=> {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
