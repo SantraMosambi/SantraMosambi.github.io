8d74ed2dac4d0f0ab2c01abab1dbe9f61cc40554
unknown <shubhamkarb18@gmail.com>
2021-10-19 22:35:06 +0530

    Start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Project";
+import Skills from "./components/Skills";
+import Home from "./components/Home"
 
 const App=()=> {
   return (
     <main>
       <Navbar />
+      <Home/>
       <About />
+       <Skills />
       <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
+      <Contact /> 
     </main>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="fs-1 fw-bold">
+        Hello, I'm<span style={{ color: "green" }}> Shubhamkar</span>
+      </div>
+      <div className="fs-2">A Web Developer</div>
+      <div>
+        I enjoy solving complex problems. Frequently praised as detail-oriented
+        by my peers, I can be relied upon to help your company achieve its goals
+        by providing sustainable and scalable solutions.
+      </div>
+    </div>
+  );
+}
