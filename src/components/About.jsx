88cb8d8e82b924f4f43ea73f24958809b3cad467
unknown <shubhamkarb18@gmail.com>
2021-10-22 23:11:27 +0530

    Start

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function About() {
   return (
-    // <div className="container my-5">
-    //   <div className="card" style={{ width: "18rem" }}>
-    //     <img src="..." className="card-img-top" alt="..." />
-    //     <div className="card-body">
-    //       <p className="card-text">
-    //         Some quick example text to build on the card title and make up the
-    //         bulk of the card's content.
-    //       </p>
-    //     </div>
-    //   </div>
-    // </div>
-    <></>
+    <div className="container my-5">
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+    </div>
+    {/* <></> */}
   );
 }
